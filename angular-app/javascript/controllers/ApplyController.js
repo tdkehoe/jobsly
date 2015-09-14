1a729825090a73c515e7ea596d0baf93bf19942b
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-09-14 13:43:32 -0600

    Comments added to database.

--- angular-app/javascript/controllers/ApplyController.js
+++
@@ -1,9 +0,0 @@
-app.controller('ApplyController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
-  $scope.message="Connected!";
-  console.log("Apply controller");
-  $http.get('http://localhost:8080/api/jobsly/' + $routeParams.id).then(function(response) { // SHOW
-    $scope.job = response.data;
-  }, function(response) {
-    console.log("Invalid URL");
-  });
-}]);
--- angular-app/javascript/controllers/ShowController.js
+++ angular-app/javascript/controllers/ShowController.js
@@ -5,4 +5,28 @@ app.controller('ShowController', ['$scope', '$http', '$routeParams', '$location'
   }, function(response) {
     console.log("Invalid URL");
   });
+
+  $scope.addJobApplication = function(job) {
+    console.log("Applying for job.");
+    console.log(job);
+    var application = {
+      applicantName: job.application.applicantName,
+      applicantEmail: job.application.applicantEmail,
+      resumeURL: job.application.resumeURL
+    };
+    console.log(application);
+    var applications = job.applications || [];
+    console.log(applications);
+    applications.push(application);
+    job.application.applicantName = null;
+    job.application.applicantEmail = null;
+    job.application.resumeURL = null;
+    job.applications = applications;
+    $http.put('http://localhost:8080/api/jobsly/' + job._id, job).then(function(response) { // UPDATE
+      console.log("Job application added.");
+    }, function(response) {
+      console.log("Invalid URL");
+    });
+  };
+
 }]);
