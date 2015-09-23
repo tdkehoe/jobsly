ed147626659efa0aee7cb340070a8ae6e92db5ff
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-09-23 09:17:35 -0600

    Fixed startup route.

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ mongod
 angular-app
 http-server -c-1 -o
 
+http://127.0.0.1:8081/#/jobsly/
+
 test with Postman
 GET http://localhost:8080/api/jobsly
--- angular-app/javascript/routes/routes.js
+++ angular-app/javascript/routes/routes.js
@@ -1,35 +1,10 @@
-// app.config(function($routeProvider) {
-//
-//   $routeProvider
-//   // should be .when('/jobsly/', {
-//   .when('/', { // INDEX
-//     templateUrl: 'javascript/templates/home.html',
-//     controller: 'HomeController'
-//   })
-//   .when('/new', { // NEW
-//     templateUrl: 'javascript/templates/new.html',
-//     controller: 'NewController'
-//   })
-//   .when('/:id/edit', { // EDIT
-//     templateUrl: 'javascript/templates/edit.html',
-//     controller: 'EditController'
-//   })
-//   .when('/:id/apply', { // APPLY
-//     templateUrl: 'javascript/templates/apply.html',
-//     controller: 'ApplyController'
-//   })
-//   .when('/:id', { // SHOW
-//     templateUrl: 'javascript/templates/show.html',
-//     controller: 'ShowController'
-//   })
-//   .otherwise({ redirectTo: '/' });
-// });
-
-
 app.config(function($routeProvider) {
 
   $routeProvider
-  // should be .when('/jobsly/', {
+  .when('/', { // INDEX
+    templateUrl: 'javascript/templates/home.html',
+    controller: 'HomeController'
+  })
   .when('/jobsly/', { // INDEX
     templateUrl: 'javascript/templates/home.html',
     controller: 'HomeController'
